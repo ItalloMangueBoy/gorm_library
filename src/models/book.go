4170dd528fe04646518146756c214f59feadaa4a
mkspop <itallomkspop@gmail.com>
2025-04-30 23:57:49 -0300

    book model

--- src/models/book.go
+++ src/models/book.go
@@ -1,12 +1,29 @@
 package models
 
+import (
+	"library/src/helpers"
+)
+
 type Book struct {
-	ID          int64    `json:"id"`
-	Pages       int64    `json:"page"`
-	Available   bool     `json:"available"`
-	Title       string   `json:"title"`
-	Author      string   `json:"author"`
-	Description *string  `json:"description"`
-	CreatedAt   string   `json:"created_at"`
-	UpdatedAt   string   `json:"updated_at"`
+	ID          int64   `json:"id"`
+	Pages       int64   `json:"page" validate:"required"`
+	Available   bool    `json:"available" validate:"required"`
+	Title       string  `json:"title" validate:"required"`
+	Author      string  `json:"author" validate:"required"`
+	Description *string `json:"description" validate:"required"`
+	CreatedAt   string  `json:"created_at"`
+	UpdatedAt   string  `json:"updated_at"`
+}
+
+// Valid detect errors in the Book struct fields
+func (b *Book) Valid() helpers.ErrorMessages {
+	customMessages := helpers.ErrorMessages{
+		"Book.Pages.required":       "Pages is required",
+		"Book.Available.required":   "Available is required",
+		"Book.Title.required":       "Title is required",
+		"Book.Author.required":      "Author is required",
+		"Book.Description.required": "Description is required",
+	}
+
+	return helpers.ValidateStruct(b, customMessages)
 }
